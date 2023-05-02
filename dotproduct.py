3dd4a739a61470f7b51e14449054caed40cac8ee
vahid <vr.ramezani@gmail.com>
2023-05-02 05:36:08 -0700

    second eound of pull requests

--- dotproduct.py
+++ dotproduct.py
@@ -1,68 +1,74 @@
-import math 
+import math
 import torch
 import torch.nn as nn
 
-#This class accepts queries, keys, and values as inputs and returns the output of the attention mechanism
-#as well as the attention weights. 
+# This class accepts queries, keys, and values as inputs and returns the output of the attention mechanism
+# as well as the attention weights. 
 
 class ScaleDotProductAttention(nn.Module):
 
-    # The constructor accepts the dimension of the key and value vectors as input.
+   
     def __init__(self, d_tensor ):
+        """
+        The constructor accepts the dimension of the key and value vectors as input.
+
+        args:
+            d_tensor (int): the dimension of the key and value feature vectors
+        """
         super().__init__()
 
-        #The square root of the dimension of the key feature vector is stored in the d_tensor attribute
-        self.d_tensor = math.sqrt(d_tensor)
+        # The square root of the dimension of the key feature vector is stored in the d_tensor attribute
+        self.scale = math.sqrt(d_tensor)
         
 
     # The forward method accepts queries, keys, and values as inputs and returns the output of the attention mechanism
     # Each tensor has the following dimensions: batch size, number of heads, number of timesteps, and dimension of the feature vector
     def forward(self, queries ,keys, values, mask=None):
+        """
+        The forward method accepts queries, keys, and values as inputs and returns the output of the attention mechanism
+        Each tensor has the following dimensions: batch size, number of heads, number of timesteps, and dimension of the feature vector
 
-        #by transposing the keys, we can compute the dot product between the queries and keys via matrix multiplication
-        #The queries and keys are multiplied together and divided by the square root of the dimension of the key vector
+        args:
+            queries (torch.Tensor): a 4D tensor with dimensions batch size, number of heads, number of timesteps, 
+            and dimension of the feature vector
+            keys (torch.Tensor): a 4D tensor with dimensions batch size, number of heads, number of timesteps, 
+            and dimension of the feature vector
+            values (torch.Tensor): a 4D tensor with dimensions batch size, number of heads, number of timesteps,
+            and dimension of the feature vector
+            mask (torch.Tensor): a mask that is used to make the attention mechanism causal with dimensions batch size,
+            number of heads, number of timesteps, and number of timesteps. The feature dimension is taken care of by
+            broadcasting. The mask is a tensor with binary values where 0 indicates that the attention weights are not.
+        """
 
-        keys_transpose=keys.transpose(2,3)
-        attention = (queries @ keys_transpose)/self.d_tensor
+        # by transposing the keys, we can compute the dot product between the queries and keys via matrix multiplication
+        # The queries and keys are multiplied together and divided by the square root of the dimension of the key vector
+        keys_transpose = keys.transpose(2,3)
+        attention = (queries @ keys_transpose)/self.scale
 
-        #The mask is used to make the attention mechanism causal
-        if mask !=None :
-            attention = attention.masked_fill(mask == 0, '-inf')
+        # The mask is used to make the attention mechanism causal
+        if mask is not None:
+            attention = attention.masked_fill(mask == 0, float('-inf'))
 
-        #The softmax function is applied to the attention weights to obtain the output of the attention mechanism
-        #The output of the attention mechanism is multiplied by the values to obtain the final output of the attention mechanism
+        # The softmax function is applied to the attention weights to obtain the output of the attention mechanism
+        # The output of the attention mechanism is multiplied by the values to obtain the final output of the attention mechanism
         attention = torch.softmax(attention, dim=-1)
-        values = attention @ values#
+        values = attention @ values
+        return values, attention
 
-        return values, attention#
+# Testing the ScaleDotProductAttention class
+if __name__ == '__main__':
 
-#Testing the ScaleDotProductAttention class
-if __name__ == "__main__":
-
-    #The queries, keys, and values are 4D tensors with the following dimensions: batch size, number of heads, number of timesteps, and dimension features
+    # The queries, keys, and values are 4D tensors with the following dimensions: batch size, number of heads, number of timesteps, and dimension features
     batch_size = 2
     heads = 3
     timesteps = 8
     feats = 4
-
     keys = torch.rand(batch_size, heads, timesteps, feats)
-    quereis = torch.rand(batch_size, heads, timesteps, feats)
+    queries = torch.rand(batch_size, heads, timesteps, feats)
     values = torch.rand(batch_size, heads, timesteps, feats)
 
-    #d_tensor is the dimension of the key and value feature vectors
+    # d_tensor is the dimension of the key and value feature vectors
     d_tensor = keys.shape[-1]
-
     test = ScaleDotProductAttention(d_tensor)
-    values, attention = test(quereis,keys,values)
+    values, attention = test(queries,keys,values)
     print(values.shape)
-
-
-
-
-
-
-    
-    
-    
-    
-
--- ecn.py
+++ ecn.py
@@ -2,52 +2,57 @@ import torch
 import torch.nn as nn
  
 
-#this class is used to compute the sinusoid encoding 
+
 class PositionalEncoding(nn.Module):
 
-    # The constructor accepts the dimension of the positional encoding and the maximum length of the sequence as input
-    # The dimension of the positional encoding is equal to the dimension of the word embedding; i.e the dimension of feature vector of the word
     def __init__(self, d_model, max_len):
+        """
+        This class is used to compute the sinusoid encoding of the positions of the words in a sequence.
+
+        args:
+            d_model (int): the dimension of the positional encoding which is equal to the 
+                dimension of the word embedding
+            max_len (int): the maximum length of the sequence
+        """
         super().__init__()
 
         # we use register_buffer to store the positional encoding as a buffer so that it is not updated during training
         # furthermore, this allows for the positional encoding to be moved to the GPU, if available with the model parameters
         self.register_buffer('encoding', torch.zeros((max_len, d_model), dtype=torch.float32))
 
-
         # set up the a range of possible values for the positions of the words in the sequence
-        # setup the argument of the sinusoid function for the even and odd indices of the positins of the words
-        # note the argument is always given by an even number, even indecies map to sin and odd indices map to cos
         pos = torch.arange(0, max_len, dtype=torch.float32)
+        # setup the argument of the sinusoid function for the even and odd indices of the positions of the words
         evenumbers = torch.arange(0, d_model, step=2, dtype=torch.float32)
+        # note the argument is always given by an even number, even indecies map to sin and odd indices map to cos
         arg = torch.outer(pos, (10000 **( -evenumbers / d_model)))
 
-        
         # compute the positional encoding
         self.encoding[:, 0::2] = torch.sin(arg)
         self.encoding[:, 1::2] = torch.cos(arg)
         
-    # The forward method accepts a batch of sequences as input and returns the positional encoding for the sequence
     def forward(self, x):
-        
+        """
+        The forward method accepts a batch of sequences as input and returns the positional encoding for the sequence
+
+        args:
+            x (torch.Tensor): a 2D tensor with dimensions batch size and sequence length
+        """
+
         # x is a 2D tensor with dimensions batch size and sequence length
         # we use the sequence length to cutoff the positional encoding to the correct length
         batch_size, seq_len = x.shape
         return self.encoding[:seq_len, :]
 
-
 # Testing the PositionalEncoding class
 # condition to run the code below
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     # The constrcutor accepts the dimension of the feature vector of the word and the maximum length of the sequence as input
-    position = PositionalEncoding (d_model=8, max_len=40)
+    position = PositionalEncoding (d_model = 8, max_len = 40)
 
     # The input to the positional encoding is a 2D tensor with 
     # we are testing with batch size of 1 and sequence length of 20
-    x = torch.arange(0, 20, dtype=torch.float32)
-    x = x.unsqueeze(dim=0)
+    x = torch.arange(0, 20, dtype = torch.float32)
+    x = x.unsqueeze(dim = 0)
     print(position.forward(x).shape)
-
-
-
